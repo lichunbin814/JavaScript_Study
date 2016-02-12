ce7e5db8d0bac8844abec425d57ca87bc30486c8
lcb814 <lcb814@gmail.com>
2016-02-12 23:48:01 +0800

    增加第二個div，做為動畫參數的範例 - 第一個動畫執行完後第二個動畫緊接著執行

--- js/main.js
+++ js/main.js
@@ -3,6 +3,20 @@ $(function () {
     $("#sample1").velocity(
         {
             width: '300px',
-            height : '300px'
+        },
+        {
+            //完成全部動畫需秏費1000豪秒(1秒)
+            duration: 1000,
         });
+
+    $("#sample2").velocity(
+    {
+        width: '300px',
+    },
+    {
+        //完成全部動畫需秏費500豪秒(0.5秒)
+        duration: 500,
+        //一秒後才執行，需搭配sample1的duration秒數，才會有連續的效果
+        delay : 1000
+    });
 })
